b5610deddb86a19f646fc1fbb1d67678f22682c3
Dhruv Patel <dhruvpatel.r78@gmail.com>
2024-10-30 17:45:04 -0400

    updated index.js for debugging

--- website/index.js
+++ website/index.js
@@ -3,7 +3,10 @@ async function updateCounter() {
     let response = await fetch(
         "https://t5shendiegekxn4lpxvnjade6i0whllr.lambda-url.us-east-1.on.aws/"
     );
+    console.log(response)
+    console.log('...............................')
     let data = await response.json();
+    console.log(data)
     counter.innerHTML = ` ${data} people have visited this page`;
 }
-await updateCounter();
+updateCounter();
