6481599fcb1d6a36d42ce8c2c836227284c341ee
Dhruv Patel <dhruvpatel.r78@gmail.com>
2024-10-30 13:29:37 -0400

    updated index.html & index,js

--- website/index.js
+++ website/index.js
@@ -17,3 +17,13 @@ const getVisitCount = () =>{
   });
   return count;
 }
+
+const counter = document.getElementById("counter");
+async function updateCounter() {
+    let response = await fetch(
+        "https://4ktzqn5mhxebbwrd454qpogcaa0jhbei.lambda-url.us-east-1.on.aws/"
+    );
+    let data = await response.json();
+    counter.innerHTML = `Thsi page has been visited by ${data} people`;
+}
+updateCounter();
