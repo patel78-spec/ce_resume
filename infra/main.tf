878b6e1dc3256092834b91dc49fbf46af3f6f5ae
Dhruv Patel <dhruvpatel.r78@gmail.com>
2024-11-02 00:06:28 -0400

    updated main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -222,4 +222,10 @@ resource "aws_lambda_function_url" "url1" {
   }
 }
 
+# CloudFront Invalidation
+resource "aws_cloudfront_distribution_invalidation" "example_invalidation" {
+  distribution_id = aws_cloudfront_distribution.s3_distribution.id
+  paths           = ["/*"]  # Invalidate all paths, or specify individual paths
+}
+
 
